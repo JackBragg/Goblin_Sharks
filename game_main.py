8f3c451135b5c1cd85123b62256d99abc61efb7e
Jack Bragg <Jack.d.bragg@gmail.com>
2019-07-22 10:45:12 -0700

    added start and end cards

--- game_main.py
+++ game_main.py
@@ -2,7 +2,25 @@ import room, item, character
 from random import randint
 from Rock_Paper_Scissors import game as RPS
 
+
+def start_card():
+	msg = ''
+	msg += '*'*60 + '\n' + '*'*60 + '\n'
+	msg += 'Welcome to our game! Move around with \'wasd\' and \'q\' to quit.\nYou will need a key to open the door. Try asking the Boss.'
+	msg += '\n' + '*'*60 + '\n' + '*'*60 + '\n'
+	print(msg)
+
+def end_card():
+	msg = ''
+	msg += '*'*60 + '\n' + '*'*60 + '\n'
+	msg += 'Thank you for playing!\n\tCaleb\n\tJack\n\tScott\n'
+	msg += '*'*60 + '\n' + '*'*60 + '\n'
+	print(msg)
+
+
 if __name__ == '__main__':
+	start_card()
+
 	hero_player = character.Player()
 	main_room = room.Room([hero_player], 10, 10)
 	main_room.spawn(hero_player, hero_player.location)
@@ -15,6 +33,7 @@ if __name__ == '__main__':
 
 	keep_playing = True
 	while keep_playing:
+		print(f'Health: {hero_player.health}')
 		if boss.check(hero_player):
 			print(f'Hello {hero_player.name}, you must defeat me in a supreme contest of skill...ROCK PAPER SCISSORS!')
 			if RPS():
@@ -28,7 +47,7 @@ if __name__ == '__main__':
 				print('AHAHAHAHAHAHAHA! I WON!\n(you take 10 damage)')
 				hero_player.health -= 10
 		if main_room.door_check(hero_player):
-			print('You opened the door, goodbye')
+			print('You opened the door!')
 
 		print(main_room)
 		inkey = input("Next command: ")
@@ -59,3 +78,8 @@ if __name__ == '__main__':
 			else:
 				hero_player.location = cur_loc
 				print('You can\'t go that way')
+		if main_room.exit:
+			if hero_player.location == main_room.exit.location:
+				keep_playing = False
+
+	end_card()
